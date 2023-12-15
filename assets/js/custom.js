e17929309c55d7095cf6039f617918f9dd5b3043
ABHISHK DINESH <abhishekdinesh44@gmail.com>
2023-12-16 04:17:28 +0530

    hkjhk

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -81,7 +81,7 @@ $(document).on("scroll", function(){
   }
 });
 
-jQuery('.gallery').featherlightGallery({ gallery: { fadeIn: 300, fadeOut: 300 }, openSpeed: 300, closeSpeed: 300 }); jQuery('.gallery2').featherlightGallery({ gallery: { next: 'next »', previous: '« previous' }, variant: 'featherlight-gallery2' });
+// jQuery('.gallery').featherlightGallery({ gallery: { fadeIn: 300, fadeOut: 300 }, openSpeed: 300, closeSpeed: 300 }); jQuery('.gallery2').featherlightGallery({ gallery: { next: 'next »', previous: '« previous' }, variant: 'featherlight-gallery2' });
 
 
 $(".gallerin").hide();
@@ -93,3 +93,5 @@ $(".gallerin").hide();
 	$(".tab-switch").removeClass("active");
 	$(this).addClass("active");
     });
+
+
