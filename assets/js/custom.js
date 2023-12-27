957bd69d2da4cd7587929afc56806ce1293bdac1
ProWebPrism <prowebprism@gmail.com>
2023-12-28 02:06:48 +0530

    dfs

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -19,6 +19,24 @@ var servSlider = new Swiper(".serv-slider", {
   autoplay: {
     delay: 5000,
   },
+  breakpoints: {
+    320:{
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    992: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+  },
 });
 
 var testswiper = new Swiper(".testiswiper", {
